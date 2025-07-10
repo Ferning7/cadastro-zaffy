939d5469afe06194c6aa9d965342fbcb833a4038
Matheus <matheusfffcorrea@gmail.com>
2025-07-10 10:43:19 -0300

    metodos de cadastro, validações de email e criptografia de senha adicionadas

--- cadastro/Usuarios.cs
+++ cadastro/Usuarios.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace cadastro
@@ -12,5 +15,123 @@ namespace cadastro
         private string email;
         private string senha;
         
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+        public string Senha
+        {
+            get { return senha; }
+            set { senha = value; }
+        }
+
+
+        public bool CadastrarUsuario()
+        {
+            try
+            {
+                using (MySqlConnection conexaoBanco = new ConexaoBD().Conectar())
+                {
+                    string senhaCripto = CriptografarSenha(Senha);
+                    string sqlInsert = "INSERT into usuarios (nome, email, senha) VALUES (@nome, @email, @senha)";
+
+                    MySqlCommand comando = new MySqlCommand(sqlInsert, conexaoBanco);
+
+                    comando.Parameters.AddWithValue("@nome", Nome);
+                    comando.Parameters.AddWithValue("@email", Email);
+                    comando.Parameters.AddWithValue("@senha", senhaCripto);
+
+                    int resultado = comando.ExecuteNonQuery();
+                    if (resultado > 0)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao cadastrar usuário ->" + ex.Message);
+                return false;
+            }
+
+        }
+
+        public bool verificarLogin()
+        {
+            try
+            {
+                using (MySqlConnection conexao = new ConexaoBD().Conectar())
+                {
+                    string senhaCripto = CriptografarSenha(Senha);
+
+                    string consultaUsuarios = "SELECT COUNT(*) FROM usuarios WHERE email = @email and senha = @senha";
+                    MySqlCommand comando = new MySqlCommand(consultaUsuarios, conexao);
+
+                    comando.Parameters.AddWithValue("@email", Email);
+                    comando.Parameters.AddWithValue("@senha", senhaCripto);
+
+                    int resultado = Convert.ToInt32(comando.ExecuteScalar());
+
+                    if (resultado > 0)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao verificar login no banco -> " + ex.Message);
+                return false;
+            }
+        }
+
+        public static string CriptografarSenha(string senha)
+        {
+            try
+            {
+                using (SHA256 sha256Hash = SHA256.Create())
+                {
+                    byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(senha));
+                    StringBuilder builder = new StringBuilder();
+                    for (int i = 0; i < bytes.Length; i++)
+                    {
+                        builder.Append(bytes[i].ToString("x2"));
+                    }
+
+                    return builder.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Não foi possível criptografar a senha: " + ex.Message, "Erro - Método Criptografar", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return "";
+            }
+        }
+
+        public static bool ValidarEmail(string email)
+        {
+            string emailValido = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
+            Regex regex = new Regex(emailValido);
+            return regex.IsMatch(email);
+        }
+
+       
     }
 }
